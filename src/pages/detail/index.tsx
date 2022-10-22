f5f26b304b32f55ab8075f8b592870c711d75efc
马文卿 <529837505@qq.com>
2022-10-22 18:56:10 +0800

    commit

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -4,10 +4,18 @@ import { useLocation } from 'react-router-dom';
 import { DetailData } from "@/types/detail";
 import { Viewer } from "@bytemd/react";
 import BgA from "@/components/BgAnimatiion";
+import gfm from "@bytemd/plugin-gfm";
+import gemoji from "@bytemd/plugin-gemoji";
+import highlight from "@bytemd/plugin-highlight-ssr";
+import mediumZoom from "@bytemd/plugin-medium-zoom";
+import mermaid from "@bytemd/plugin-mermaid";
+import frontmatter from "@bytemd/plugin-frontmatter";
 import "./index.less";
 import 'bytemd/dist/index.min.css';
 import "highlight.js/styles/vs.css";
 import 'juejin-markdown-themes/dist/juejin.min.css';
+
+const plugins = [gfm(), gemoji(), highlight(), mediumZoom(), mermaid(), frontmatter()]; 
 
 const ArticleDetail: FC = () => {
     const { state } = useLocation();
@@ -32,9 +40,10 @@ const ArticleDetail: FC = () => {
             <main className="detail_main">
                 <div className="article_main">
                     <h1 className="article_title">{articleData?.title}</h1>
-                    <Viewer value={articleData?.content as string} />
+                    <Viewer value={articleData?.content as string} plugins={plugins} />
                 </div>
-                <div className="side_box"></div>
+                <div className="side_box">
+                </div>
             </main>
         </Fragment>
     )
