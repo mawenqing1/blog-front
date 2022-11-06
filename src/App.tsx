1adc206c6e564db5241055c78be138881227cf21
马文卿 <529837505@qq.com>
2022-11-06 22:29:02 +0800

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React, { Fragment, Suspense, useState } from 'react'
+import React, { Fragment, Suspense } from 'react'
 import BlogRouter from '@/router'
 import LoadingPage from './components/Loading'
 import './App.less'
 
 function App () {
-  const [count, setCount] = useState(0)
 
   return (
     <Fragment>
