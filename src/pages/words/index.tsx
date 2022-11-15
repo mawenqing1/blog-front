9afff7f65013fdbb1b005ee31f82158bece3a087
马文卿 <529837505@qq.com>
2022-11-15 12:45:37 +0800

    add: leave words page

---
+++ src/pages/words/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC, Fragment } from "react";
+
+const LeaveWords: FC = () => {
+
+    return (
+        <Fragment>
+            
+        </Fragment>
+    )
+}
+
+export default LeaveWords
