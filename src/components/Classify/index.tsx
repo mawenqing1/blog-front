9b5a1f93ab53fd9407acafd5d87cf6b1151b1223
马文卿 <529837505@qq.com>
2022-11-16 12:56:00 +0800

    debug

--- src/components/Classify/index.tsx
+++ src/components/Classify/index.tsx
@@ -21,19 +21,19 @@ const ClassifyCard: FC<Props> = ({selectTag}: Props) => {
         }
     };
 
-    const renderList = () => {
-        // onClick={() => selectTag(el?.tag)}
-        return (
-            <Fragment>
-                {list?.map(el => (
-                    <div className={styles.classify_list} key={el?.tag} >
-                        <span className={styles.classify_type}>{returnTagName(el?.tag || '')}</span>
-                        <span className={styles.classify_num}>{el?.cnt}</span>
-                    </div>
-                ))}
-            </Fragment>
-        )
-    }
+    // const renderList = () => {
+    //     // onClick={() => selectTag(el?.tag)}
+    //     return (
+    //         <Fragment>
+    //             {list?.map(el => (
+    //                 <div className={styles.classify_list} key={el?.tag} >
+    //                     <span className={styles.classify_type}>{returnTagName(el?.tag || '')}</span>
+    //                     <span className={styles.classify_num}>{el?.cnt}</span>
+    //                 </div>
+    //             ))}
+    //         </Fragment>
+    //     )
+    // }
 
     return (
         <Fragment>
